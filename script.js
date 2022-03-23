196be2f4dce02b2a0060973c93f0e10b379dd705
Iyad Rozan Bone <54940157+iyadrozan@users.noreply.github.com>
2022-03-23 09:47:56 +0800

    Final Commit
    Final Commit All Files

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ function add() {
   //1.Mengambil Nilai yang Dituliskan Oleh User
   let newText = document.getElementById('new_text');
   //2.Membuat List Baru ke dalam UL
-  let newTodo =
-    '<li class="list-group-item"> <span id="txt-inpt" class="li-txt">' + newText.value + '</span> <span onclick="toggle()"> <i class="bi bi-check icn-chk"></i>' + '</span>' + '<span> <i class="bi bi-trash icn-dlt"></i> </span>' + '</li>';
-
+  let newTodo = '<li class="list-group-item"> <span onclick="toggle(this)" class="txt">' + newText.value + '</span>' + "<span onclick='removeBtn(this)'> <i class='bi bi-x-square-fill icn-dlt'></i></span>";
   todo.insertAdjacentHTML('afterbegin', newTodo);
   //3.Mengosongkan Fieldnya
   newText.value = '';
 }
 
-function toggle() {
-  var tg = document.getElementById('txt-inpt');
-  tg.classList.toggle('done');
+function toggle(el) {
+  el.classList.toggle('done');
+}
+
+function removeBtn(el) {
+  el.parentElement.remove();
 }
 // Fitur Yang Ditambahkan
 // 1.Menambahkan
